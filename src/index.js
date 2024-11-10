94e99f9ac469d8d859bdd5afa457444c3f1bd94d
PedroLSena <pes.ex20000@gmail.com>
2024-11-10 20:49:53 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express =  require("express");
 const path = require("node:path");
+const { execPath } = require("node:process");
+
 
 const app = express();
 const PORT = 8080;
@@ -8,19 +10,43 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 //setar a pasta views
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static("Style"));
 
 app.use(express.urlencoded({extended:true}));
 
+let emails = [];
 
 app.get("/list", (req, res)=>{
     res.render("list");
     const emailuser = req.body.emailuser;
     console.log(emailuser);
     res.redirect("/list");
-})
-//renderizar a pagina index.js em views
-app.get("/", (req, res)=>{
+});
+app.post("/signup", (req, res)=>{
+    const {email} = req.body;
+    if(email){
+        emails.push(email);
+        res.redirect("/sucsses");
+    }else{
+        res.redirect("/");
+    }
+});
+
+app.get("/sucsses",(req, res)=>{
+    res.render("sucsses");
+});
+
+app.get("/",(req, res)=>{
     res.render("index");
+})
+
+app.get("/emails", (req, res)=>{
+    res.render("emails", {emails:emails});
+})
+app.post("/emails/delete", (req, res)=>{
+    const {email} = req.body;
+    emails - emails.filter(i => i !== email);
+    res.redirect("/emails")
 })
 
 //deixar o server on
